cf7ec4257b6871cbcafc3854a80a2fa6270a52c2
Mirsmog <tanharka.tyuchiev4@gmail.com>
2023-10-16 20:36:10 +0500

    fix image path

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -6,25 +6,26 @@ import { animeApi, useGetSearchedAnimeQuery } from '../services/animeApi';
 import { Link } from 'react-router-dom';
 import store from '../redux/store';
 import MainCard from './mainCard';
-import clearIcon from '../assets/clear.svg';
+import clearIcon from '../assets/clear.svg'
 
 interface SearchProps {
   searchRef: React.RefObject<HTMLDivElement>;
   visible: boolean;
 }
-
+  
 const Search: FC<SearchProps> = ({ searchRef, visible }) => {
   const [value, setValue] = React.useState('');
   const inputRef = React.useRef<HTMLInputElement | null>(null);
   const dispatch = useAppDispatch();
-  const searchValue = useAppSelector((state) => state.filter.present.searchValue);
-  const prevSearchValue = useAppSelector((state) =>
-    state.filter.past.length > 0 ? state.filter.past[state.filter.past.length - 1].searchValue : ''
-  );
+  const searchValue = useAppSelector(state => state.filter.present.searchValue)
+  const prevSearchValue = useAppSelector(
+    state => state.filter.past.length > 0
+      ? state.filter.past[state.filter.past.length - 1].searchValue
+      : '')
 
   // TODO: Избавится от пустова QUERY запроса при загруски страницы
   // Пример. const { data, isFetching } = useGetSearchedAnimeQuery(searchValue, { skip: !searchValue })
-  const { data, isFetching, isLoading } = useGetSearchedAnimeQuery(searchValue);
+  const { data, isFetching, isLoading } = useGetSearchedAnimeQuery(searchValue)
   const resultList = data?.results ? data.results : [];
   const hasNextPage = data?.hasNextPage ? data.hasNextPage : false;
 
@@ -40,57 +41,50 @@ const Search: FC<SearchProps> = ({ searchRef, visible }) => {
 
   const onClearInput = () => {
     setValue('');
-    dispatch(setSearchValue(''));
+    dispatch(setSearchValue(''))
     inputRef.current?.focus();
   };
 
   const checkClearDate = () => {
     if (searchValue !== prevSearchValue && searchValue !== '') {
       store.dispatch(animeApi.util.resetApiState());
-      dispatch(setPage(1));
-      dispatch(setSearchPage(1));
+      dispatch(setPage(1))
+      dispatch(setSearchPage(1))
     }
-  };
+  }
 
   return (
-    <div className='relative' ref={searchRef}>
+    <div className='relative'
+      ref={searchRef}
+    >
       <input
         value={value}
         onChange={(e) => onInput(e)}
         placeholder='Search...'
-        className={`w-[400px] focus:border-info border-2 z-30 relative rounded-xl bg-slate-600 outline-none px-12 py-2 text-xl bg-no-repeat bg-[length:25px] bg-[center_left_14px] bg-[url("${clearIcon}")] placeholder:text-lg border-slate-500`}
+        className={`w-[400px] focus:border-info border-2 z-30 relative rounded-xl bg-slate-600 outline-none px-12 py-2 text-xl bg-no-repeat bg-[length:25px] bg-[center_left_14px] bg-[url("./assets/search.svg")] placeholder:text-lg border-slate-500`}
       />
       {value && !isFetching && (
         <img
           onClick={onClearInput}
           className='absolute bottom-0 right-0 h-[48px] opacity-70 hover:opacity-100 cursor-pointer z-30'
-          src='/src/assets/clear.svg'
+          src='./assets/clear.svg'
           alt='clear'
         />
       )}
-      {!isLoading && isFetching && (
-        <span className='loading loading-spinner loading-md text-gray-400 absolute top-3 right-3 z-30'></span>
-      )}
-      <div
-        className={`rounded-xl absolute bg-slate-700 w-full top-[82%] z-20 p-6 pr-0 shadow-slate-900 drop-shadow-md shadow-xl rounded-t-none border-slate-600 border-2 overflow-hidden transition-opacity duration-300 ${
-          (!visible || resultList.length == 0) && 'opacity-0 h-0 py-0'
-        } ${hasNextPage ? 'pb-2' : ''} searchTarget`}
-      >
+      {!isLoading && isFetching &&
+        <span className="loading loading-spinner loading-md text-gray-400 absolute top-3 right-3 z-30"></span>
+      }
+      <div className={`rounded-xl absolute bg-slate-700 w-full top-[82%] z-20 p-6 pr-0 shadow-slate-900 drop-shadow-md shadow-xl rounded-t-none border-slate-600 border-2 overflow-hidden transition-opacity duration-300 ${(!visible || resultList.length == 0) && 'opacity-0 h-0 py-0'} ${hasNextPage ? 'pb-2' : ''} searchTarget`}>
         <ul className={`flex flex-wrap gap-8 justify-center max-h-[520px] overflow-y-auto `}>
-          {resultList &&
-            resultList.map((item) => <MainCard isSearchCard={true} key={item.id} {...item} />)}
+          {resultList && resultList.map((item) =>
+            <MainCard isSearchCard={true} key={item.id} {...item} />
+          )}
         </ul>
-        {hasNextPage && (
-          <Link
-            onClick={checkClearDate}
-            to={`search/${searchValue}`}
-            className='showAll inline-block pt-2 text-md text-info hover:text-indigo-500 hover:underline'
-          >
-            show All
-          </Link>
-        )}
+        {
+          hasNextPage && <Link onClick={checkClearDate} to={`search/${searchValue}`} className='showAll inline-block pt-2 text-md text-info hover:text-indigo-500 hover:underline'>show All</Link>
+        }
       </div>
-    </div>
+    </div >
   );
 };
 export default Search;
