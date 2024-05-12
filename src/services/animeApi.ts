cfc0a449542a6a3f71fa0463376017ea02cb17f6
Zwine <tanharka.tyuchiev4mail.ru>
2024-05-13 04:12:49 +0500

    fix api url

--- src/services/animeApi.ts
+++ src/services/animeApi.ts
@@ -1,35 +1,35 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { IGetData } from "@/models/anime.model";
-import { AnimeInfo } from "@/services/types/AnimeInfo";
-import { animeEpisode } from "@/services/types/AnimeEpisode";
-import { Anime } from "@/services/types/Anime";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { IGetData } from '@/models/anime.model';
+import { AnimeInfo } from '@/services/types/AnimeInfo';
+import { animeEpisode } from '@/services/types/AnimeEpisode';
+import { Anime } from '@/services/types/Anime';
 
 export const animeApi = createApi({
-  reducerPath: "animeApi",
+  reducerPath: 'animeApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://manga-project-ten.vercel.app/anime/gogoanime/",
+    baseUrl: 'https://anilist-api-wheat.vercel.app/anime/gogoanime/',
   }),
-  tagTypes: ["Bookmarks"],
+  tagTypes: ['Bookmarks'],
   endpoints: (build) => ({
     getSearchedAnime: build.query<IGetData, string>({
       query: (value) => `${value}`,
     }),
 
     getAnimeInfo: build.query<AnimeInfo, string>({
-      query: (id = "") => `info/${id}`,
+      query: (id = '') => `info/${id}`,
     }),
     getAnimeEpisode: build.query<animeEpisode, string>({
-      query: (episodeId = "") => ({
+      query: (episodeId = '') => ({
         url: `/watch/${episodeId}`,
-        params: { server: "mp4upload" },
-        method: "GET",
+        params: { server: 'mp4upload' },
+        method: 'GET',
       }),
     }),
 
     getAnime: build.query<IGetData, { page?: number; genre?: string }>({
-      query: ({ page = 1, genre = "" }) => ({
+      query: ({ page = 1, genre = '' }) => ({
         url: genre ? `genre/${genre}?page=${page}` : `top-airing?page=${page}`,
-        method: "GET",
+        method: 'GET',
       }),
 
       serializeQueryArgs: ({ endpointName }) => {
@@ -56,9 +56,9 @@ export const animeApi = createApi({
     }),
 
     getSearchedAll: build.query<IGetData, { query?: string; page?: number }>({
-      query: ({ query = "", page = 1 }) => ({
+      query: ({ query = '', page = 1 }) => ({
         url: `${query}?page=${page}`,
-        method: "GET",
+        method: 'GET',
       }),
 
       serializeQueryArgs: ({ endpointName }) => {
@@ -86,26 +86,26 @@ export const animeApi = createApi({
 
     getBookmarks: build.query<Anime[], void>({
       query: () => ({
-        url: "https://65266250917d673fd76c20bc.mockapi.io/favorites",
-        method: "GET",
+        url: 'https://65266250917d673fd76c20bc.mockapi.io/favorites',
+        method: 'GET',
       }),
-      providesTags: () => ["Bookmarks"],
+      providesTags: () => ['Bookmarks'],
     }),
 
     addBookmark: build.mutation<Anime, Anime>({
       query: (data) => ({
-        url: "https://65266250917d673fd76c20bc.mockapi.io/favorites",
-        method: "POST",
+        url: 'https://65266250917d673fd76c20bc.mockapi.io/favorites',
+        method: 'POST',
         body: data,
       }),
-      invalidatesTags: ["Bookmarks"],
+      invalidatesTags: ['Bookmarks'],
     }),
     deleteBookmark: build.mutation<string, string | undefined>({
-      query: (mockId = "") => ({
+      query: (mockId = '') => ({
         url: `https://65266250917d673fd76c20bc.mockapi.io/favorites/${mockId}`,
-        method: "DELETE",
+        method: 'DELETE',
       }),
-      invalidatesTags: ["Bookmarks"],
+      invalidatesTags: ['Bookmarks'],
     }),
   }),
 });
